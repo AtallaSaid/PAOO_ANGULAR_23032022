7a9f25f0464261ae86c577612c58b057b28e3210
César Okada <cesar.okada@fatec.sp.gov.br>
2022-03-23 18:04:35 -0300

    Primeiro app em Angular

---
+++ src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ola-angular',
+  templateUrl: './ola-angular.component.html',
+  styleUrls: []
+})
+export class OlaAngularComponent{
+
+  framework = 'React'
+
+}
